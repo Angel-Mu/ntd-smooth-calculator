7687c299c9da57babd0980858b6dd9ed13e99dd7
Angel Malavar <angel.malavar@gmail.com>
2024-08-07 09:59:21 -0600

    feat(heroku): add procfile for heroku deployment

--- api/config/index.js
+++ api/config/index.js
@@ -4,7 +4,7 @@ const sequelizeConfig = require('./sequelize');
 const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    API_PORT: Joi.number().default(3000),
+    PORT: Joi.number().default(3000),
     DEFAULT_PAGE_SIZE: Joi.number().default(50),
     SERVER_JWT_SECRET: Joi.string().required(),
     DEFAULT_JWT_EXPIRATION: Joi.number().default(3600),
@@ -20,7 +20,7 @@ if (error) {
 
 const config = {
   env: envVars.NODE_ENV,
-  port: envVars.API_PORT,
+  port: envVars.PORT,
   defaultPageSize: envVars.DEFAULT_PAGE_SIZE,
   jwtSecret: envVars.SERVER_JWT_SECRET,
   jwtExpiration: envVars.DEFAULT_JWT_EXPIRATION,
